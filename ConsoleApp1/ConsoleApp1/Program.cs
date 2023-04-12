fd489a25f78e48f8d0e13950dce2da9b6d52a4b1
ndtThinh <130574889+ndtThinh@users.noreply.github.com>
2023-04-12 22:05:04 +0700

    Update Program.cs
    helle thinh

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Minh Minh!");
+            Console.WriteLine("Hello Thinh!");
         }
     }
 }
